e6b8f629ccad10c0c6142ca762b71671556e0d5d
Nayzos <natjha08@gmail.com>
2022-05-03 13:53:40 -0700

    enemy changed to triggerenter

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -47,7 +47,7 @@ public class EnemyController : MonoBehaviour
         }
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
